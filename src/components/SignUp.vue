9880830e52351ecba921647b7fbf98f9158a84bc
zarlinaingtun <zarlinaingtun1999@gmail.com>
2021-08-03 12:46:35 -0700

    loopSkillAnddeleteSkill

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -9,7 +9,8 @@
              <label for="password">Password</label>
              <input type="password" class="password" v-model="password"/>
           </div>
-
+          
+          <!-- Select box -->
           <div class="form-control">
                <label for="role">Role</label>
                 <select id="role" v-model="role">
@@ -17,12 +18,14 @@
                     <option value="designer">Web Designer</option>
                 </select>
           </div>
-
+            
+            <!-- Single Checkbox -->
             <div class="accept">
                 <input type="checkbox" id="policy" v-model="accept"/>
                 <label for="policy">Accept terms and conditions</label>
             </div>
-
+            
+            <!-- Multiple Checkbox -->
             <div class="programLanguage">
                 <div class="program">
                     <input type="checkbox" value="python" v-model="names"/>
@@ -38,18 +41,23 @@
                     <input type="checkbox" value="php" v-model="names"/>
                     <label>Php</label>
                 </div>
-
             </div>
+
+            <!-- Radio Button -->
             <div class="gendergp">
                 <input type="radio" id="male" value="male" v-model="gender"/>
                 <label for="male">Male</label>
                 <input type="radio" id="female" value="female" v-model="gender"/>
                 <label for="female">Female</label>
             </div>
-
+            
+            <!-- Input text with Keyup Event -->
             <div class="form-control">
                 <label for="skill">Skills</label>
-                <input type="text" id="skill" @keyup="addSkills" v-model="skill"/>
+                <input type="text" id="skill" @keyup.alt="addSkill" v-model="skill"/>
+                <div v-for="skill in skills" :key="skill">
+                    <p>{{skill}}<span class="cross" @click="deleteSkill(skill)">&#10060;</span></p>
+                </div>
             </div>
             
         
@@ -63,7 +71,8 @@
   <h3>Selected Programming Language:{{names}}</h3>
   <h3>Gender-{{gender}}</h3>
   <h3>Skill-{{skill}}</h3>
-  <h3>SkillsArray-{{skills}}</h3>
+  <h3>Skills-{{skills}}</h3>
+  
 </template>
 
 <script>
@@ -81,12 +90,20 @@ export default {
         }
     },
     methods:{
-        addSkills(e){
-        //    console.log(e.key);
-        if(e.key===','){
+        addSkill(e){
+        console.log(e.key);
+        if(e.key===',' && this.skill){
             this.skills.push(this.skill);
             this.skill=''
         }
+        },
+        deleteSkill(skill){
+            // console.log(skill);
+
+            //return Array skillData(except delete data)
+            this.skills=this.skills.filter(loopskill=>{
+                return loopskill!=skill;//html != php(delete data)
+            });
         }
     }
 }
@@ -132,6 +149,10 @@ export default {
      }
      .gendergp{
          margin: 5px 0px;
+     }
+     .cross{
+         cursor:pointer;
+         padding:2px;
      }
     
 </style>
